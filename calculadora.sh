34bbc32fe56966c24c4f17fa7c55b0fea1e74c94
ArthMazzini <augusto.mazzini21@gmail.com>
2024-12-04 16:08:36 -0300

    script calculadora sh
    script da calculadora com códigos em python

---
+++ calculadora.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Digite o primeiro número: "
+read num1
+echo "Digite o segundo número:"
+read num2
+
+echo "Escolha a operação (+, -, *, /):"
+read operacao
+
+case $operacao in
++) resultado=$(echo "$num1 + $num2" | bc);;
+-) resultado=$(echo "$num1 - $num2" | bc);;
+\*)resultado=$(echo "$num1 * $num2" | bc);;
+/)resultado=$(echo "scale=4; $num1 / $num2" | bc);; 
+*)echo "Operação inválida"; exit 1;;
+esac
+
+echo "Resultado: $resultado"
+
+
